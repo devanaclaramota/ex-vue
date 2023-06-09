ec105adef2fa237ba02f3a01d43dd605f1381977
Ana clara <dev.anaclaramota@outlook.com>
2023-06-08 23:03:28 -0300

    commit-inicial

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { reactive } from 'vue';
+
+import Header from './components/Header.vue';
+import Calculadora from './components/Calculadora.vue';
+
+
+const estado = reactive({
+  resultado: 0,
+  primeiroValor: 0,
+  segundoValor: 0,
+  operador: ""
+});
+
+const igual = () => {
+  switch(estado.operador) {
+      case "soma":
+        estado.resultado = parseInt(estado.primeiroValor) + parseInt(estado.segundoValor)
+        break;
+      case "subtracao":
+        estado.resultado = parseInt(estado.primeiroValor) - parseInt(estado.segundoValor);
+        break;
+      case "multiplicacao":
+        estado.resultado = parseInt(estado.primeiroValor) * parseInt(estado.segundoValor);
+        break;
+      case "divisao":
+        estado.resultado = parseInt(estado.primeiroValor) / parseInt(estado.segundoValor);
+        break;
+  
+  }
+}
+</script>
+
+<template>
+  
+  <Header />
+
+  <Calculadora
+    :primeiro-valor="evento => estado.primeiroValor = evento.target.value"
+    :segundo-valor="evento => estado.segundoValor = evento.target.value"
+    :operador="evento => estado.operador = evento.target.value"
+    :resultado="estado.resultado"
+    :igual="igual()"
+    
+  />
+</template>
+
+
+
+<style scoped>
+
+
+</style>
